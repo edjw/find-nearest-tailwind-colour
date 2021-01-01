a1e9a4a45d242dcf3347dd8fb49ec1fe4f590eba
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2021-01-01 06:59:52 +0000

    add readme, make hex mark optional

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -20,14 +20,10 @@
 </svelte:head>
 
 <header class="p-4 mt-1 -ml-4 -mr-4 bg-cyan-800 sm:rounded">
-	<h1 class="text-2xl leading-none text-cyan-50 ">
-		Find the nearest Tailwind colour
-	</h1>
+	<h1 class="leading-none text-cyan-50 ">Find the nearest Tailwind colour</h1>
 	<p class="mt-2 text-cyan-100">
 		A tool to find the nearest colour from
-		<a
-			href="https://tailwindcss.com/docs/customizing-colors"
-			class="underline">
+		<a href="https://tailwindcss.com/docs/customizing-colors">
 			the default Tailwind colour palette</a>
 	</p>
 </header>
@@ -40,19 +36,19 @@
 	<section class="mt-10">
 		<p class="font-semibold">Nearest Tailwind Colour</p>
 		{#if $userColour != null && isHexColour($userColour)}
-			<p class="mt-4">
+			<p>
 				<span class="text-gray-700">Tailwind colour name: </span>
 				<span
 					class="w-48 px-4 py-1 mt-4 mb-2 font-semibold leading-loose underline border shadow-sm border-cyan-500">
 					{nearestTailwindColour['name']}</span>
 			</p>
 
-			<p class="mt-4 leading-loose text-gray-700">
+			<p class="leading-loose text-gray-700 ">
 				Hex colour code:
 				{nearestTailwindColour['value']}
 			</p>
 
-			<p class="mt-4 text-gray-700">Preview</p>
+			<p class="text-gray-700 ">Preview</p>
 
 			<div class="w-48 h-20">
 				<ColourBlock
@@ -67,16 +63,13 @@
 </main>
 
 <footer
-	class="p-4 mt-8 mb-1 -ml-4 -mr-4 sm:rounded text-cyan-50 bg-cyan-800 border-top-4">
-	<p class="mt-2">
+	class="p-4 mt-8 -ml-4 -mr-4 sm:rounded text-cyan-50 bg-cyan-800 border-top-4">
+	<p class="mt-0">
 		Made by
-		<a href="https://twitter.com/_edjw" class="underline">Ed
-			Johnson-Williams</a>
+		<a href="https://twitter.com/_edjw">Ed Johnson-Williams</a>
 	</p>
-	<p class="mt-2">
-		<a
-			class="underline"
-			href="https://github.com/edjw/find-nearest-tailwind-colour">See the
+	<p>
+		<a href="https://github.com/edjw/find-nearest-tailwind-colour">See the
 			code for this website</a>
 	</p>
 </footer>
