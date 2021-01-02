cc67393adbda01c5ae20e23e5bada44a3633ef67
Ed Johnson-Williams <edjw@users.noreply.github.com>
2021-01-02 09:07:17 +0000

    remove padding from top

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -6,6 +6,6 @@
 </style>
 
 <div
-    class="container grid h-screen px-4 pt-4 mx-auto max-w-prose grid-cols-full grid-rows-auto-1-auto">
+    class="container grid h-screen px-4 mx-auto max-w-prose grid-cols-full grid-rows-auto-1-auto">
     <slot />
 </div>
