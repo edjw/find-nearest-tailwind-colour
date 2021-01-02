55cf4fff3ecac65227a503dee3d0f9fd020f7159
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2021-01-02 10:00:33 +0000

    remove unused <style> tags to stop client.css loading

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -2,30 +2,8 @@
 	export let status;
 	export let error;
 
-	const dev = process.env.NODE_ENV === 'development';
+	const dev = process.env.NODE_ENV === "development";
 </script>
-
-<style>
-	h1, p {
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
 
 <svelte:head>
 	<title>{status}</title>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -2,9 +2,6 @@
     export const segment = "";
 </script>
 
-<style>
-</style>
-
 <div
     class="container grid h-screen px-4 mx-auto max-w-prose grid-cols-full grid-rows-auto-1-auto">
     <slot />
