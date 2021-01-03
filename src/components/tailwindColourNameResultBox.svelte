ca0abbfcfd31973908942e6eb47c91a073886047
Ed Johnson-Williams <edjw@users.noreply.github.com>
2021-01-03 13:30:51 +0000

    tighten margin on result box

--- src/components/tailwindColourNameResultBox.svelte
+++ src/components/tailwindColourNameResultBox.svelte
@@ -16,7 +16,7 @@
 </script>
 
 <span
-    class="w-48 px-4 py-1 mt-4 mb-2 font-semibold leading-loose underline border shadow cursor-pointer border-cyan-500 block"
+    class="w-48 px-4 py-1 mt-2 mb-2 font-semibold leading-loose underline border shadow cursor-pointer border-cyan-500 block"
     title="Click to copy"
     on:click={copyColourNameToClipboard}>
     {resultText}
