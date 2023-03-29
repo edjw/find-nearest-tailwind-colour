9fd730af9b42b00325bb692bd09dd42799033a75
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-03-30 00:03:20 +0100

    add a modal to explain the version numbers switcher

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,18 +1,22 @@
 <script>
+	import TailwindVersionHelp from './TailwindVersionHelp.svelte';
 	export let tailwindVersion = ''; // Can be v3.3+, v3.2 or v2
+
+	let dialog;
 </script>
 
 <header class="p-4 mt-1 -ml-4 -mr-4 bg-cyan-800 sm:rounded">
 	<h1 class="leading-none text-cyan-50">Find the nearest Tailwind colour</h1>
 
+	<TailwindVersionHelp bind:dialog />
 	{#if tailwindVersion === 'v3.3+'}
 		<p class="mt-2 text-cyan-100">
 			A tool to find the nearest colour from
-			<a href="https://tailwindcss.com/docs/customizing-colors">
-				the Tailwind {tailwindVersion} colour palette</a
-			>
+			<a href="https://tailwindcss.com/docs/customizing-colors"> the Tailwind colour palette</a>
 		</p>
-		<p class="mt-2 text-cyan-100">
+		<div class="flex flex-col sm:flex-row mt-2 gap-y-2 gap-x-4" />
+		<p class="mt-0 text-cyan-100">
+			Switch Tailwind version:
 			<a href="/3-2">Tailwind v3.2</a> |
 			<a href="/2">Tailwind v2</a>
 		</p>
@@ -22,10 +26,11 @@
 		<p class="mt-2 text-cyan-100">
 			A tool to find the nearest colour from
 			<a href="https://tailwindcss.com/docs/customizing-colors">
-				the Tailwind {tailwindVersion} colour palette</a
+				the <strong>Tailwind {tailwindVersion}</strong> colour palette</a
 			>
 		</p>
 		<p class="mt-2 text-cyan-100">
+			Switch Tailwind version:
 			<a href="/">Tailwind v3.3+</a> |
 			<a href="/2">Tailwind v2</a>
 		</p>
@@ -35,12 +40,17 @@
 		<p class="mt-2 text-cyan-100">
 			A tool to find the nearest colour from
 			<a href="https://v2.tailwindcss.com/docs/customizing-colors">
-				the Tailwind {tailwindVersion} colour palette</a
+				the <strong>Tailwind {tailwindVersion}</strong> colour palette</a
 			>
 		</p>
 		<p class="mt-2 text-cyan-100">
+			Switch Tailwind version:
 			<a href="/">Tailwind v3.3+</a> |
 			<a href="/3-2">Tailwind v3.2</a>
 		</p>
 	{/if}
+
+	<div class="text-cyan-50">
+		<button on:click={() => dialog.showModal()}>What?</button>
+	</div>
 </header>
---
+++ src/components/TailwindVersionHelp.svelte
@@ -0,0 +1,21 @@
+<script>
+	export let dialog;
+</script>
+
+<dialog class="md:w-1/2" bind:this={dialog} on:close on:click={() => dialog.close()}>
+	<button
+		class="absolute top-2 right-2 px-2 font-bold border"
+		on:click={() => dialog.close()}
+		on:cancel={() => dialog.close()}
+	>
+		×
+	</button>
+	<div class="mt-6 mr-2 md:mr-6">
+		<p>
+			Tailwind has 3 different versions of its colour palette. So the closest Tailwind colour to
+			your hexcode may vary depending on which version of Tailwind you're using.
+		</p>
+		<p>Tailwind 3.3 introduced more shades in addition to the Tailwind 3.0-3.2 colour palette.</p>
+		<p>Tailwind 3 added many colours to the palette from Tailwind 2.</p>
+	</div>
+</dialog>
