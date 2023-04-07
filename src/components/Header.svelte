db2eb1b49f4c23a5b297b2ec9211114fcdb45143
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-04-07 08:47:55 +0100

    make version selection a bit dry-er; put version selector inside a details

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { page } from '$app/stores';
+
 	import TailwindVersionHelp from './TailwindVersionHelp.svelte';
 	export let tailwindVersion = ''; // Can be v3.3+, v3.2 or v2
 
@@ -9,48 +11,37 @@
 	<h1 class="leading-none text-cyan-50">Find the nearest Tailwind colour</h1>
 
 	<TailwindVersionHelp bind:dialog />
-	{#if tailwindVersion === 'v3.3+'}
-		<p class="mt-2 text-cyan-100">
-			A tool to find the nearest colour from
+
+	<p class="mt-2 text-cyan-100">
+		A tool to find the nearest colour from
+		{#if tailwindVersion === 'v3.3+'}
 			<a href="https://tailwindcss.com/docs/customizing-colors"> the Tailwind colour palette</a>
-		</p>
-		<div class="flex flex-col sm:flex-row mt-2 gap-y-2 gap-x-4" />
-		<p class="mt-0 text-cyan-100">
-			Switch Tailwind version:
-			<a href="/3-2">Tailwind v3.2</a> |
-			<a href="/2">Tailwind v2</a>
-		</p>
-	{/if}
+		{/if}
 
-	{#if tailwindVersion === 'v3.2'}
-		<p class="mt-2 text-cyan-100">
-			A tool to find the nearest colour from
-			<a href="https://tailwindcss.com/docs/customizing-colors">
-				the <strong>Tailwind {tailwindVersion}</strong> colour palette</a
+		{#if tailwindVersion === 'v3.2'}
+			<a
+				href="https://web.archive.org/web/20230318012140/https://tailwindcss.com/docs/customizing-colors"
 			>
-		</p>
-		<p class="mt-2 text-cyan-100">
-			Switch Tailwind version:
-			<a href="/">Tailwind v3.3+</a> |
-			<a href="/2">Tailwind v2</a>
-		</p>
-	{/if}
-
-	{#if tailwindVersion === 'v2'}
-		<p class="mt-2 text-cyan-100">
-			A tool to find the nearest colour from
+				the <strong>Tailwind v3.0-3.2</strong> colour palette</a
+			>
+		{/if}
+		{#if tailwindVersion === 'v2'}
 			<a href="https://v2.tailwindcss.com/docs/customizing-colors">
 				the <strong>Tailwind {tailwindVersion}</strong> colour palette</a
 			>
-		</p>
-		<p class="mt-2 text-cyan-100">
-			Switch Tailwind version:
-			<a href="/">Tailwind v3.3+</a> |
-			<a href="/3-2">Tailwind v3.2</a>
-		</p>
-	{/if}
+		{/if}
+	</p>
+	<div class="flex flex-col sm:flex-row mt-2 gap-y-2 gap-x-4" />
+	<div class="mt-0 text-cyan-100">
+		<details>
+			<summary class="cursor-pointer">Switch Tailwind version</summary>
 
-	<div class="text-cyan-50">
-		<button on:click={() => dialog.showModal()}>What?</button>
+			<a href="/" class:font-bold={$page.url.pathname === '/'}>Tailwind v3.3+</a> |
+			<a href="/3-2" class:font-bold={$page.url.pathname === '/3-2'}>Tailwind v3.0-3.2</a> |
+			<a href="/2" class:font-bold={$page.url.pathname === '/2'}>Tailwind v2</a> |
+			<button on:click={() => dialog.showModal()}>What?</button>
+		</details>
 	</div>
+
+	<div class="text-cyan-50" />
 </header>
