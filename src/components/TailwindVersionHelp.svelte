7be6bc86c96780006cd261dc86e00559854ef849
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-04-07 08:47:21 +0100

    slight copy improvement to version explanation

--- src/components/TailwindVersionHelp.svelte
+++ src/components/TailwindVersionHelp.svelte
@@ -11,9 +11,10 @@
 		×
 	</button>
 	<div class="mt-6 mr-2 md:mr-6">
+		<p>Tailwind has 3 different versions of its colour palette.</p>
 		<p>
-			Tailwind has 3 different versions of its colour palette. So the closest Tailwind colour to
-			your hexcode may vary depending on which version of Tailwind you're using.
+			So the closest Tailwind colour to your hexcode <em>may</em> vary depending on which version of
+			Tailwind you're using.
 		</p>
 		<p>Tailwind 3.3 introduced more shades in addition to the Tailwind 3.0-3.2 colour palette.</p>
 		<p>Tailwind 3 added many colours to the palette from Tailwind 2.</p>
