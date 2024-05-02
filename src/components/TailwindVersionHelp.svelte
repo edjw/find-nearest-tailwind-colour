5f9d829bbff019747dd246541307dbccc81f792e
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2024-05-02 22:39:08 +0100

    little bit of animation on copying the value

--- src/components/TailwindVersionHelp.svelte
+++ src/components/TailwindVersionHelp.svelte
@@ -1,39 +1,58 @@
 <script>
 	export let dialog;
+
+	function closeDialog() {
+		dialog.close();
+	}
 </script>
 
-<dialog class="md:w-1/2 rounded-lg" bind:this={dialog} on:close on:click={() => dialog.close()}>
-	<button
-		class="absolute top-2 right-2 px-2 font-bold border"
-		on:click={() => dialog.close()}
-		on:cancel={() => dialog.close()}
-	>
-		×
-	</button>
-	<div class="p-12">
-		<p>Tailwind has had 3 different versions of its colour palette.</p>
-		<p>
-			So the closest Tailwind colour to your hexcode <em>may</em> vary depending on which version of
-			Tailwind you're using.
-		</p>
-		<p>Tailwind 3.3 added more shades to the Tailwind 3.0-3.2 colour palette.</p>
-		<p>Tailwind 3.0 added many colours to the palette from Tailwind 2.</p>
+<dialog
+	class="md:w-1/2 rounded-lg"
+	bind:this={dialog}
+	on:close={() => {}}
+	on:click|self={() => closeDialog()}
+	on:keydown={(event) => {
+		if (event.key === "Escape") {
+			closeDialog();
+		}
+	}}
+>
+	<div class="dialog-content" role="dialog" aria-modal="true">
+		<button
+			class="absolute top-2 right-2 px-4 py-2 rounded font-bold border"
+			on:click={closeDialog}
+		>
+			×
+		</button>
+		<div class="p-12">
+			<p>Tailwind has had 3 different versions of its colour palette.</p>
+			<p>
+				So the closest Tailwind colour to your hexcode <em>may</em> vary depending on which version of
+				Tailwind you're using.
+			</p>
+			<p>Tailwind 3.3 added more shades to the Tailwind 3.0-3.2 colour palette.</p>
+			<p>Tailwind 3.0 added many colours to the palette from Tailwind 2.</p>
 
-		<p class="font-medium">See the various Tailwind colour palettes</p>
+			<p class="font-medium">See the various Tailwind colour palettes</p>
 
-		<ul class="list-disc list-inside mt-2 flex flex-col gap-y-2">
-			<li>
-				<a href="https://tailwindcss.com/docs/customizing-colors" target="_blank">Tailwind v3.3+</a>
-			</li>
-			<li>
-				<a
-					href="https://web.archive.org/web/20230318012140/https://tailwindcss.com/docs/customizing-colors"
-					target="_blank">Tailwind v3.0-3.2</a
-				> (Web Archive snapshot)
-			</li>
-			<li>
-				<a href="https://v2.tailwindcss.com/docs/customizing-colors" target="_blank">Tailwind v2</a>
-			</li>
-		</ul>
+			<ul class="list-disc list-inside mt-2 flex flex-col gap-y-2">
+				<li>
+					<a href="https://tailwindcss.com/docs/customizing-colors" target="_blank"
+						>Tailwind v3.3+</a
+					>
+				</li>
+				<li>
+					<a
+						href="https://web.archive.org/web/20230318012140/https://tailwindcss.com/docs/customizing-colors"
+						target="_blank">Tailwind v3.0-3.2</a
+					> (Web Archive snapshot)
+				</li>
+				<li>
+					<a href="https://v2.tailwindcss.com/docs/customizing-colors" target="_blank"
+						>Tailwind v2</a
+					>
+				</li>
+			</ul>
+		</div>
 	</div>
 </dialog>
--- src/components/tailwindColourNameResultBox.svelte
+++ src/components/tailwindColourNameResultBox.svelte
@@ -1,39 +1,67 @@
 <script>
+	import { fade } from "svelte/transition";
 	export let resultText;
+
+	export let copied = false;
 
 	const copyColourNameToClipboard = async () => {
 		if (!navigator.clipboard) {
-			// Clipboard API not available
 			return;
 		}
 
 		try {
 			await navigator.clipboard.writeText(resultText);
+			copied = true;
+			setTimeout(() => {
+				copied = false;
+			}, 4000);
 		} catch (err) {
 			console.error("Failed to copy!", err);
 		}
 	};
 </script>
 
-<span
-	class="flex justify-between w-48 px-4 py-1 mt-2 mb-2 border shadow cursor-pointer border-cyan-500"
+<button
+	class="w-48 border shadow cursor-pointer py-2.5 px-4"
 	title="Click to copy"
 	on:click={copyColourNameToClipboard}
 >
-	<span class="font-semibold leading-loose underline">
-		{resultText}
-	</span>
-	<svg
-		class="w-5 h-5 mt-2"
-		fill="none"
-		stroke="currentColor"
-		viewBox="0 0 24 24"
-		xmlns="http://www.w3.org/2000/svg"
-		><path
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			stroke-width="1.5"
-			d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
-		/></svg
-	>
-</span>
+	{#if copied}
+		<div class="flex justify-between items-center" in:fade={{ duration: 500 }}>
+			<span class="font-semibold"> Copied! </span>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-5 h-5"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+				/>
+			</svg>
+		</div>
+	{:else}
+		<div class="flex justify-between items-center" in:fade={{ duration: 500 }}>
+			<span class="font-semibold underline">
+				{resultText}
+			</span>
+			<svg
+				class="w-5 h-5"
+				fill="none"
+				stroke="currentColor"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="1.5"
+					d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
+				/></svg
+			>
+		</div>
+	{/if}
+</button>
