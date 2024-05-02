9a2099fe1d9e3d37c684ac3df3b6758d5990367d
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2024-05-02 21:45:09 +0100

    add custom store to ensure colour always starts with an #

--- src/scripts/colourStore.js
+++ src/scripts/colourStore.js
@@ -1,5 +1,30 @@
 import { writable } from "svelte/store";
 
-const colour = writable("");
+function createColourStore() {
+	const { subscribe, set, update } = writable("");
+
+	return {
+		subscribe,
+		set: (value) => {
+			if (value.startsWith("#")) {
+				set(value);
+			} else {
+				set("#" + value);
+			}
+		},
+		update: (updater) => {
+			update((currentValue) => {
+				const newValue = updater(currentValue);
+				if (newValue.startsWith("#")) {
+					return newValue;
+				} else {
+					return "#" + newValue;
+				}
+			});
+		}
+	};
+}
+
+const colour = createColourStore();
 
 export { colour };
